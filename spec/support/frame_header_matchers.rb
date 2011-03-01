cb77fcaaff0ad55b19bdbb014a440444244857eb
Ian D. Eccles <ian.eccles@gmail.com>
2011-03-01 13:26:31 -0500

    cleaned up rspec differences between 1.8.7 and 1.9.2 using custom matchers

--- spec/support/frame_header_matchers.rb
+++ spec/support/frame_header_matchers.rb
@@ -8,3 +8,28 @@ end
 RSpec::Matchers.define :have_transaction_header do |expected|
   have_frame_header :transaction, expected
 end
+
+RSpec::Matchers.define :have_command do |expected|
+  match do |actual|
+    actual.command.should == expected
+  end
+end
+
+RSpec::Matchers.define :have_body_encoding do |expected|
+  if RUBY_VERSION >= "1.9"
+    match do |actual|
+      actual.body.encoding.name.should == expected
+    end
+  else
+    match do |actual|
+      true.should be_true
+    end
+  end
+end
+
+RSpec::Matchers.define :have_body do |expected, expected_no_encoding, encoding|
+  e_expected = (RUBY_VERSION >= '1.9') ? expected.encode(encoding) : expected_no_encoding
+  match do |actual|
+    actual.body.should == e_expected
+  end
+end
